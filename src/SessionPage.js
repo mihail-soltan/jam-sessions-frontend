3e562ab5de2689897baa118cd0d45ec82f4a21b0
mihail-soltan <mihail.soltan@protonmail.ch>
2022-01-15 00:43:46 +0200

    create add participant to session btn

--- src/SessionPage.js
+++ src/SessionPage.js
@@ -5,6 +5,7 @@ import SyncLoader from "react-spinners/SyncLoader";
 import "./SessionPage.css";
 import { Link } from "react-router-dom";
 import { IoIosSend } from "react-icons/io";
+import axios from "axios";
 function uuidv4() {
   return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
     (
@@ -14,47 +15,46 @@ function uuidv4() {
   );
 }
 
-
-export default function SessionPage({ loading, search, newData, authToken, me }) {
-
-
+export default function SessionPage({
+  loading,
+  search,
+  newData,
+  authToken,
+  me,
+}) {
   const jamSessionID = useParams();
+  const sessionAPI = `https://jam-sessions-backend.herokuapp.com/jamsessions/${jamSessionID.id}`;
   const filteredSession = search.filter(
     (session) => session._id === jamSessionID.id
   );
-  console.log(jamSessionID);
-  console.log(filteredSession);
-  //   console.log(showSession);
-  //   const body = marked(showSession.recipe || "");
+
   const [color, setColor] = useState("#F2AA4C");
   const override = css`
     display: block;
     margin: 0 auto;
     border-color: red;
   `;
-    console.log(me)
-  // const participate = () => {
-  //   filteredSession
-  // }
-
-  const participate = (e) => {
-    console.log(e.target)
-  }
-
-  const [participateBtn, setParticipateBtn] = useState({
-    className: 'participate-Disabled',
-    disabled: true
-})
-
-console.log(participateBtn.className)
-  useEffect(() =>{
-    if(authToken){
-      setParticipateBtn({
-        className: 'participate-btn',
-        disabled: false,
+  
+  const participate = () => {
+    axios.put(sessionAPI, {
+      ...filteredSession[0],
+      members: [...filteredSession[0].members, me[0].data._id]
     })
+  };
+  console.log(filteredSession[0].members)
+  const [participateBtn, setParticipateBtn] = useState({
+    className: "participate-Disabled",
+    disabled: true,
+  });
+
+  useEffect(() => {
+    if (authToken) {
+      setParticipateBtn({
+        className: "participate-btn",
+        disabled: false,
+      });
     }
-  }, [])
+  }, []);
 
   return search ? (
     <>
@@ -62,7 +62,6 @@ console.log(participateBtn.className)
         {filteredSession.map((s) => (
           <>
             {" "}
-            {/* <h3>Created by {s.createdBy}</h3> */}
             <div className="sessionCard1">
               <div className="sessionTitle">
                 {" "}
@@ -73,8 +72,14 @@ console.log(participateBtn.className)
                   {new Date(s.date).toString().substring(0, 15)}
                 </h5>
               </div>
+              <div>
+                <h5>
+                  Session created by: {s.createdBy.firstName}{" "}
+                  {s.createdBy.lastName}
+                </h5>
+              </div>
               <div className="sessionMain">
-                <h4 key={uuidv4()}>{s.description} </h4>
+                <p key={uuidv4()}>{s.description} </p>
               </div>
               {authToken ? (
                 <form>
@@ -91,17 +96,23 @@ console.log(participateBtn.className)
                 <h1>Please sign in to send a message</h1>
               )}
               <div className="sessionDetails">
-              <div className="sessionAddress">
-                <h6 className="cardAddress" key={uuidv4()}>
-                  {s.street}
-                </h6>
-                <h6 className="cardAddress" key={uuidv4()}>
-                  {s.plz}
-                </h6>
+                <div className="sessionAddress">
+                  <h6 className="cardAddress" key={uuidv4()}>
+                    {s.street}
+                  </h6>
+                  <h6 className="cardAddress" key={uuidv4()}>
+                    {s.plz}
+                  </h6>
                 </div>
                 <div className="participants">
-                <h6> Participants: {s.members.length}/4</h6>
-                <button disabled={participateBtn.disabled}  onClick={participate} className={participateBtn.className}>Participate</button>
+                  <h6> Participants: {s.members.length}/4</h6>
+                  <button
+                    disabled={participateBtn.disabled}
+                    onClick={participate}
+                    className={participateBtn.className}
+                  >
+                    Participate
+                  </button>
                 </div>
               </div>
               <div className="cardBottom">
